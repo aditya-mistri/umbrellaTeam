b89134a768915d7466537c65527db7ec266703b0
aditya <aditya@mait>
2025-04-01 10:36:06 +0530

    feat: implement user authentication retrieval and enhance Navbar with user details and sign-out functionality

--- client/src/components/Sidebar/index.tsx
+++ client/src/components/Sidebar/index.tsx
@@ -2,6 +2,7 @@
 import React, { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { signOut } from "aws-amplify/auth";
 import {
   Lock,
   Home,
@@ -22,60 +23,73 @@ import {
 import { usePathname } from "next/navigation";
 import { useAppDispatch, useAppSelector } from "../../app/redux";
 import { setIsSidebarCollapsed } from "../../app/state/index";
-import { useGetProjectsQuery } from "../../app/state/api";
+import { useGetAuthUserQuery, useGetProjectsQuery } from "../../app/state/api";
+import { usePathname } from "next/navigation";
 
 const Sidebar = () => {
-  const [showProjects, setShowProjects] = useState(false);
-  const [showPriority, setShowPriority] = useState(false);
+  const [showProjects, setShowProjects] = useState(true);
+  const [showPriority, setShowPriority] = useState(true);
 
   const { data: projects } = useGetProjectsQuery();
-  console.log(projects);
   const dispatch = useAppDispatch();
   const isSidebarCollapsed = useAppSelector(
     (state) => state.global.isSidebarCollapsed,
   );
 
-  const sidebarClassNames = `fixed flex flex-col h-full justify-between shadow-xl
-    transition-all duration-300 z-40 dark:bg-black overflow-y-auto bg-white 
-    ${isSidebarCollapsed ? "hidden" : "w-64"}`;
+  const { data: currentUser } = useGetAuthUserQuery({});
+  const handleSignOut = async () => {
+    try {
+      await signOut();
+    } catch (error) {
+      console.error("Error signing out: ", error);
+    }
+  };
+  if (!currentUser) return null;
+  const currentUserDetails = currentUser?.userDetails;
+
+  const sidebarClassNames = `fixed flex flex-col h-[100%] justify-between shadow-xl
+    transition-all duration-300 h-full z-40 dark:bg-black overflow-y-auto bg-white
+    ${isSidebarCollapsed ? "w-0 hidden" : "w-64"}
+  `;
 
   return (
     <div className={sidebarClassNames}>
-      <div className="flex h-full w-full flex-col justify-start">
+      <div className="flex h-[100%] w-full flex-col justify-start">
         {/* TOP LOGO */}
-        <div className="z-50 flex min-h-[56px] w-full items-center justify-between bg-white p-6 pt-3 dark:bg-black">
+        <div className="z-50 flex min-h-[56px] w-64 items-center justify-between bg-white px-6 pt-3 dark:bg-black">
           <div className="text-xl font-bold text-gray-800 dark:text-white">
-            EDlist
+            EDLIST
           </div>
-          {/* Close Sidebar Button */}
-          {!isSidebarCollapsed && (
+          {isSidebarCollapsed ? null : (
             <button
-              className="py-2"
-              onClick={() =>
-                dispatch(setIsSidebarCollapsed(!isSidebarCollapsed))
-              }
+              className="py-3"
+              onClick={() => {
+                dispatch(setIsSidebarCollapsed(!isSidebarCollapsed));
+              }}
             >
-              <X className="h-6 w-6 text-gray-800 hover:text-gray-600 dark:text-white" />
+              <X className="h-6 w-6 text-gray-800 hover:text-gray-500 dark:text-white" />
             </button>
           )}
         </div>
         {/* TEAM */}
         <div className="flex items-center gap-5 border-y-[1.5px] border-gray-200 px-8 py-4 dark:border-gray-700">
-          <Image 
-          src="https://ut-s3-images.s3.ap-south-1.amazonaws.com/logo.png" 
-          width={40} height={40} alt="Team" />
+          <Image
+            src="https://ut-s3-images.s3.ap-south-1.amazonaws.com/logo.png"
+            alt="Logo"
+            width={40}
+            height={40}
+          />
           <div>
-            <h3 className="text-md font-bold tracking-wide dark:text-white">
-              {"Aditya's Team"}
+            <h3 className="text-md font-bold tracking-wide dark:text-gray-200">
+              EDROH TEAM
             </h3>
-
             <div className="mt-1 flex items-start gap-2">
-              <Lock className="mt-[0.1rem] h-3 w-3 text-gray-500 dark:text-gray-400" />
+              <LockIcon className="mt-[0.1rem] h-3 w-3 text-gray-500 dark:text-gray-400" />
               <p className="text-xs text-gray-500">Private</p>
             </div>
           </div>
         </div>
-        {/* Navbar Links */}
+        {/* NAVBAR LINKS */}
         <nav className="z-10 w-full">
           <SidebarLink icon={Home} label="Home" href="/" />
           <SidebarLink icon={Briefcase} label="Timeline" href="/timeline" />
@@ -85,37 +99,35 @@ const Sidebar = () => {
           <SidebarLink icon={Users} label="Teams" href="/teams" />
         </nav>
 
-        {/* Projects Links */}
+        {/* PROJECTS LINKS */}
         <button
           onClick={() => setShowProjects((prev) => !prev)}
-          className="text-gray flex w-full items-center justify-between px-8 py-3"
+          className="flex w-full items-center justify-between px-8 py-3 text-gray-500"
         >
-          <span>Projects</span>
+          <span className="">Projects</span>
           {showProjects ? (
             <ChevronUp className="h-5 w-5" />
           ) : (
             <ChevronDown className="h-5 w-5" />
           )}
         </button>
-        {/* Projects List */}
-        {showProjects && (
-          <>
-            {projects?.map((project) => (
-              <SidebarLink
-                key={project.id}
-                icon={Briefcase}
-                label={project.name}
-                href={`/project/${project.id}`}
-              />
-            ))}
-          </>
-        )}
-        {/* Priorities Links */}
+        {/* PROJECTS LIST */}
+        {showProjects &&
+          projects?.map((project) => (
+            <SidebarLink
+              key={project.id}
+              icon={Briefcase}
+              label={project.name}
+              href={`/projects/${project.id}`}
+            />
+          ))}
+
+        {/* PRIORITIES LINKS */}
         <button
           onClick={() => setShowPriority((prev) => !prev)}
-          className="text-gray flex w-full items-center justify-between px-8 py-3"
+          className="flex w-full items-center justify-between px-8 py-3 text-gray-500"
         >
-          <span>Priority</span>
+          <span className="">Priority</span>
           {showPriority ? (
             <ChevronUp className="h-5 w-5" />
           ) : (
@@ -148,34 +160,60 @@ const Sidebar = () => {
           </>
         )}
       </div>
+      <div className="z-10 mt-32 flex w-full flex-col items-center gap-4 bg-white px-8 py-4 dark:bg-black md:hidden">
+        <div className="flex w-full items-center">
+          <div className="align-center flex h-9 w-9 justify-center">
+            {!!currentUserDetails?.profilePictureUrl ? (
+              <Image
+                src={`https://ut-s3-images.s3.ap-south-1.amazonaws.com/${currentUserDetails?.profilePictureUrl}`}
+                alt={currentUserDetails?.username || "User Profile Picture"}
+                width={100}
+                height={50}
+                className="h-full rounded-full object-cover"
+              />
+            ) : (
+              <User className="h-6 w-6 cursor-pointer self-center rounded-full dark:text-white" />
+            )}
+          </div>
+          <span className="mx-3 text-gray-800 dark:text-white">
+            {currentUserDetails?.username}
+          </span>
+          <button
+            className="self-start rounded bg-blue-400 px-4 py-2 text-xs font-bold text-white hover:bg-blue-500 md:block"
+            onClick={handleSignOut}
+          >
+            Sign out
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
 
 interface SidebarLinkProps {
   href: string;
-  icon: React.ElementType;
+  icon: LucideIcon;
   label: string;
 }
 
 const SidebarLink = ({ href, icon: Icon, label }: SidebarLinkProps) => {
   const pathname = usePathname();
-  const isActive = pathname === href;
+  const isActive =
+    pathname === href || (pathname === "/" && href === "/dashboard");
 
   return (
     <Link href={href} className="w-full">
       <div
-        className={`relative flex cursor-pointer items-center justify-start gap-3 transition-colors hover:bg-gray-100 dark:hover:bg-gray-700 ${
-          isActive
-            ? "bg-gray-100 text-gray-800 dark:bg-gray-600 dark:text-white"
-            : ""
-        } px-8 py-3`}
+        className={`relative flex cursor-pointer items-center gap-3 transition-colors hover:bg-gray-100 dark:bg-black dark:hover:bg-gray-700 ${
+          isActive ? "bg-gray-100 text-white dark:bg-gray-600" : ""
+        } justify-start px-8 py-3`}
       >
         {isActive && (
-          <div className="absolute left-0 top-0 h-full w-[5px] bg-blue-700" />
+          <div className="absolute left-0 top-0 h-[100%] w-[5px] bg-blue-200" />
         )}
-        <Icon className="h-5 w-5 text-gray-600 dark:text-gray-400" />
-        <span className="text-sm font-medium text-gray-800 dark:text-gray-100">
+
+        <Icon className="h-6 w-6 text-gray-800 dark:text-gray-100" />
+        <span className={`font-medium text-gray-800 dark:text-gray-100`}>
           {label}
         </span>
       </div>
